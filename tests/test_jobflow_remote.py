dc041b56348a1ed09d90b2e06977eb496f3d8af6
Matthew Evans <git@ml-evs.science>
2023-10-11 13:33:10 +0100

    Add a second dummy test that checks top-level imports

--- tests/test_jobflow_remote.py
+++ tests/test_jobflow_remote.py
@@ -3,3 +3,13 @@ from jobflow_remote import __version__
 
 def test_version():
     assert __version__ == "0.0.1"
+
+
+def test_imports():
+    """This test triggers all the top-level imports by importing
+    the global `SETTINGS`.
+
+    """
+    from jobflow_remote import SETTINGS  # noqa
+
+    ...
