6dafdd29a5978dc551b922288042cb5318ee62be
Matthew Evans <7916000+ml-evs@users.noreply.github.com>
2024-01-29 22:25:20 +0000

    Add test for jobs with callable kwargs (#51)

--- src/jobflow_remote/testing/__init__.py
+++ src/jobflow_remote/testing/__init__.py
@@ -1,4 +1,5 @@
 """A series of toy workflows that can be used for testing."""
+from typing import Callable, Optional, Union
 
 from jobflow import job
 
@@ -20,3 +21,15 @@ def write_file(n):
     with open("results.txt", "w") as f:
         f.write(str(n))
     return
+
+
+@job
+def arithmetic(
+    a: Union[float, list[float]],
+    b: Union[float, list[float]],
+    op: Optional[Callable] = None,
+) -> Optional[float]:
+    if op:
+        return op(a, b)
+
+    return None
