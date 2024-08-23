831ed4a074837efa84e37666f13fa7613280f6e6
Guido Petretto <g.petretto@gmail.com>
2024-08-23 12:05:22 +0200

    Update src/jobflow_remote/remote/host/remote.py
    Co-authored-by: David Waroquiers <david.waroquiers@matgenix.com>

--- src/jobflow_remote/remote/host/remote.py
+++ src/jobflow_remote/remote/host/remote.py
@@ -323,7 +323,7 @@ class RemoteHost(BaseHost):
         self._execute_remote_func(self.connection.sftp().remove, str(path))
 
     def rmtree(self, path: str | Path, raise_on_error: bool = False) -> bool:
-        """Recursively delete a directory tree on a host.
+        """Recursively delete a directory tree on a remote host.
 
         It is intended to remove an entire directory tree, including all files
         and subdirectories, on the host represented by the subclass.
