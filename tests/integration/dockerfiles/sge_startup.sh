c146999f402167483515026ba4e3e733577b70c7
Matthew Evans <git@ml-evs.science>
2024-09-23 13:54:42 +0100

    Tweak SGE startup script misleading output

--- tests/integration/dockerfiles/sge_startup.sh
+++ tests/integration/dockerfiles/sge_startup.sh
@@ -9,10 +9,12 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 # Run the SGE installation scripts at startup as the docker network is not initialised during build
+# Both install scripts will report errors, as systemd is not present and will fail to start them,
+# so we redirect the output to avoid confusion and manually launch the services
 ${sudo_cmd} bash <<SCRIPT
-cd /opt/sge && yes "" | ./install_qmaster
+cd /opt/sge && yes "" | ./install_qmaster 2>/dev/null
 /opt/sge/default/common/sgemaster start
-cd /opt/sge && yes "" | ./install_execd
+cd /opt/sge && yes "" | ./install_execd 2>/dev/null
 /opt/sge/default/common/sgeexecd start
 source /opt/sge/default/common/settings.sh
 qconf -as $HOSTNAME
