5fcda6075460889a19a4fe11cab54cae0bd114af
Guido Petretto <g.petretto@gmail.com>
2024-09-25 15:06:15 +0200

    add mock tests

---
+++ tests/db/remote/host/test_remote.py
@@ -0,0 +1,40 @@
+from unittest.mock import MagicMock, patch
+
+
+@patch("fabric.Connection.run")
+@patch("fabric.Connection.cd")
+def test_sanitize(mock_cd, mock_run):
+    from jobflow_remote.remote.host.base import SANITIZE_KEY
+    from jobflow_remote.remote.host.remote import RemoteHost
+
+    rh = RemoteHost(
+        host="localhost",
+        retry_on_closed_connection=False,
+        sanitize=True,
+        shell_cmd=None,
+    )
+    rh._check_connected = lambda: True
+
+    cmd = "echo 'test'"
+
+    echo_cmd = f'echo -n "{SANITIZE_KEY}" | tee >(cat >&2)'
+    expected_cmd = f"{echo_cmd};{cmd};{echo_cmd}"
+    mock_stdout = f"SOME NOISE --{SANITIZE_KEY}test{SANITIZE_KEY}SOME appended TEXT"
+
+    # Configure the mock
+    mock_cd.return_value.__enter__ = (
+        MagicMock()
+    )  # This makes the context manager do nothing
+    mock_cd.return_value.__exit__ = MagicMock()
+    mock_run.return_value.stdout = mock_stdout
+    mock_run.return_value.stderr = ""
+
+    # Call the function that uses subprocess.run
+    stdout, stderr, _ = rh.execute(cmd)
+
+    # Assert that subprocess.run was called with the expected arguments
+    mock_run.assert_called_once_with(expected_cmd, timeout=None, hide=True, warn=True)
+
+    # Assert on the result of your function
+    assert stdout == "test"
+    assert stderr == ""
