f845bb95263cec1c9c910cd24a40470d6c4269fc
David Waroquiers <david.waroquiers@matgenix.com>
2025-02-18 18:30:21 +0100

    Marking "root" jobflow_remote test as unit test.

--- tests/test_jobflow_remote.py
+++ tests/test_jobflow_remote.py
@@ -1,9 +1,14 @@
+import pytest
+
+
+@pytest.mark.unit()
 def test_version() -> None:
     from jobflow_remote import __version__
 
     assert __version__.startswith("0."), f"unexpected {__version__=}"
 
 
+@pytest.mark.unit()
 def test_imports() -> None:
     """This test triggers all the top-level imports by importing
     the global `SETTINGS`.
