06489a1c7cf7bc9e9db4e568f80530553db1d290
Guido Petretto <g.petretto@gmail.com>
2025-06-17 10:13:06 +0200

    bugfix and test release workflow

--- src/jobflow_remote/jobs/runner.py
+++ src/jobflow_remote/jobs/runner.py
@@ -275,15 +275,9 @@ class Runner:
                 self.checkout
             )
 
-        if transfer or queue or complete:
-            try:
-                self.advance_state(states)
-            except Exception:
-                logger.exception("Error during initial advance_state")
-            scheduler.every(self.runner_options.delay_advance_status).seconds.do(
-                self.advance_state, states=states
-            )
-
+        # first perform this step with check run status and update the status of
+        # the limited workers (if any). Otherwise, upon start of the runner the initial
+        # count of the currently submitted jobs is 0.
         if queue:
             try:
                 self.check_run_status()
@@ -315,6 +309,15 @@ class Runner:
                 scheduler.every(self.runner_options.delay_update_batch).seconds.do(
                     self.update_batch_jobs
                 )
+
+        if transfer or queue or complete:
+            try:
+                self.advance_state(states)
+            except Exception:
+                logger.exception("Error during initial advance_state")
+            scheduler.every(self.runner_options.delay_advance_status).seconds.do(
+                self.advance_state, states=states
+            )
 
         # all the processes will ping the running_runner document to signal
         # that at least one is still active.
@@ -359,11 +362,6 @@ class Runner:
         self.checkout()
         scheduler.every(self.runner_options.delay_checkout).seconds.do(self.checkout)
 
-        self.advance_state(states)
-        scheduler.every(self.runner_options.delay_advance_status).seconds.do(
-            self.advance_state, states=states
-        )
-
         self.check_run_status()
         scheduler.every(self.runner_options.delay_check_run_status).seconds.do(
             self.check_run_status
@@ -386,6 +384,11 @@ class Runner:
             scheduler.every(self.runner_options.delay_update_batch).seconds.do(
                 self.update_batch_jobs
             )
+
+        self.advance_state(states)
+        scheduler.every(self.runner_options.delay_advance_status).seconds.do(
+            self.advance_state, states=states
+        )
 
         running_states = [
             JobState.READY.value,
