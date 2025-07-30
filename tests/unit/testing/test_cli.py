399ebc314091150d8966e0c3b6852ac894598652
David Waroquiers <david.waroquiers@matgenix.com>
2025-07-30 09:53:16 +0200

    Few tweaks on run_check_cli. Added tests for run_check_cli. Added possibility to test the color.

---
+++ tests/unit/testing/test_cli.py
@@ -0,0 +1,56 @@
+import pytest
+from typer.testing import Result
+
+from jobflow_remote.testing.cli import run_check_cli
+
+
+def test_run_check_cli():
+    # Check multiple lines is found
+    flow_excerpt = """├── flow: Commands for managing the flows
+│   ├── delete: Permanently delete Flows from the database
+│   ├── graph: Provide detailed information on a Flow.
+│   ├── info: Provide detailed information on a Flow.
+│   ├── list: Get the list of Flows in the database."""
+    res = run_check_cli(
+        ["--tree"], required_out=["flow: Commands for managing the flows", flow_excerpt]
+    )
+    assert isinstance(res, Result)
+    assert res.exit_code == 0
+
+    # Check exclusion
+    with pytest.raises(
+        AssertionError, match=r"flow: Commands for managing the flows present in stdout"
+    ):
+        run_check_cli(
+            ["--tree"], excluded_out=["flow: Commands for managing the flows"]
+        )
+
+    # Check with some additional spaces
+    with pytest.raises(
+        AssertionError,
+        match=r"flow:   Commands for managing the \nflows present in stdout",
+    ):
+        run_check_cli(
+            ["--tree"], excluded_out=["flow:   Commands for managing the \nflows"]
+        )
+
+    run_check_cli(["--tree"], required_out_colored="[bold red]jf[/bold red]")
+
+    with pytest.raises(AssertionError):
+        run_check_cli(["--tree"], required_out_colored="[green]jf[/green]")
+
+    run_check_cli(
+        ["--tree"],
+        required_out_colored="[bold green]job[/bold green]: Commands for   managing the jobs",
+    )
+
+    with pytest.raises(AssertionError):
+        run_check_cli(
+            ["--tree"], required_out_colored="job: Commands for managing the jobs"
+        )
+
+    with pytest.raises(AssertionError):
+        run_check_cli(
+            ["--tree"],
+            required_out_colored="[bold green]job[/bold green]: Commands [red]for[/red] managing the jobs",
+        )
