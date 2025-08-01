71843893ca71d7f41f3960d44849e22ecdc20f71
David Waroquiers <david.waroquiers@matgenix.com>
2025-08-01 16:55:23 +0200

    Hopefully final!

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -205,10 +205,11 @@ services:
                 )
                 print(" * Copying coverage data back...")
                 coverage_container_paths = []
+                # This needs to be set as it may have been set elsewhere
+                os.environ["COVERAGE_FILE"] = ".coverage"
                 for c in containers:
                     if c.name in ("mongo_container",):
                         continue
-                    print(f"GETTING BACK FROM container {c.name}")
                     coverage_container_dir = integration_cov_dir / c.name
                     coverage_container_dir.mkdir(exist_ok=True)
                     coverage_container_paths.append(coverage_container_dir)
@@ -217,80 +218,26 @@ services:
                     ).splitlines()
                     for file in flist:
                         if file.startswith(".coverage"):
-                            print("DBG coverage file:", file)
                             c.copy_from(
                                 f"/home/jobflow/coverage/{file}",
                                 coverage_container_dir / file,
                             )
                     with cd(coverage_container_dir):
-                        print("IN coverage_container_dir :", coverage_container_dir)
-                        print("BEFORE combine:")
-                        print(os.listdir(coverage_container_dir))
-                        print(os.listdir("."))
-                        print("COVERAGE FILE ENVIRONMENT VARIABLE")
-                        print(os.environ["COVERAGE_FILE"])
-                        os.environ["COVERAGE_FILE"] = ".coverage"
+                        # Combining coverage from the different jf execution runs for all the tests on this container
                         cov = Coverage()
                         cov.combine()
                         cov.save()
-                        with open("somerandomfile.txt", "w") as fff:
-                            fff.write("SOME text ...")
-                        print("AFTER combine and save:")
-                        print(os.listdir(coverage_container_dir))
-                        print(os.listdir("."))
                 with cd(integration_cov_dir):
-                    # cov = Coverage()
-                    # data_paths = [
-                    #     p.relative_to(integration_cov_dir) / ".coverage"
-                    #     for p in coverage_container_paths
-                    # ]
-                    # print("DATA PATHS:")
-                    # print(data_paths)
-                    #
-                    # data_paths = [str(p) for p in data_paths if p.exists()]
-                    # cov.combine(data_paths=data_paths, keep=True)
-                    # cov.save()
-                    # print("INTEGRATION COV DIR AFTER combine and save")
-                    # print(os.listdir(integration_cov_dir))
-                    # for directory in os.listdir(integration_cov_dir):
-                    #     print(f"List of files in directory {directory}")
-                    #     if os.path.isdir(directory):
-                    #         print(os.listdir(integration_cov_dir / directory))
-                    #     else:
-                    #         print("... not a directory!!")
-                    # shutil.move(
-                    #     ".coverage", coverage_dir / ".coverage-integration-remote"
-                    # )
-                    for directory in os.listdir(integration_cov_dir):
-                        print(f"List of files in directory {directory}")
-                        if os.path.isdir(directory):
-                            print(os.listdir(integration_cov_dir / directory))
-                        else:
-                            print("... not a directory!!")
                     for cov_container_path in coverage_container_paths:
                         cov_dir = cov_container_path.relative_to(integration_cov_dir)
                         cov_file = cov_dir / ".coverage"
                         shutil.copy(cov_file, f".coverage.{cov_dir}")
 
+                    # Combining the coverage from each container
                     cov = Coverage()
-                    # data_paths = [
-                    #     p.relative_to(integration_cov_dir) / ".coverage"
-                    #     for p in coverage_container_paths
-                    # ]
-                    # print("DATA PATHS:")
-                    # print(data_paths)
-                    #
-                    # data_paths = [str(p) for p in data_paths if p.exists()]
                     cov.combine()
                     cov.save()
-                    # print("INTEGRATION COV DIR AFTER combine and save")
-                    # print(os.listdir(integration_cov_dir))
-                    # for directory in os.listdir(integration_cov_dir):
-                    #     print(f"List of files in directory {directory}")
-                    #     if os.path.isdir(directory):
-                    #         print(os.listdir(integration_cov_dir / directory))
-                    #     else:
-                    #         print("... not a directory!!")
+
                     shutil.move(
                         ".coverage", coverage_dir / ".coverage-integration-remote"
                     )
