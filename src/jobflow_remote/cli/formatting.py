7b33652024e943ab3f7f8f441c98ab3adf044e43
David Waroquiers <david.waroquiers@matgenix.com>
2025-10-23 11:43:09 +0200

    Added limits for the metadata shown. Handle jobs_info None case.

--- src/jobflow_remote/cli/formatting.py
+++ src/jobflow_remote/cli/formatting.py
@@ -10,6 +10,7 @@ from typing import TYPE_CHECKING
 from monty.json import jsanitize
 from rich.markdown import Markdown
 from rich.panel import Panel
+from rich.pretty import Pretty
 from rich.scope import render_scope
 from rich.table import Table
 from rich.text import Text
@@ -120,7 +121,10 @@ header_name_data_getter_map = {
         if ji.lock_time
         else None,
     ),
-    "metadata": ("Metadata", lambda ji: render_scope(ji.metadata)),
+    "metadata": (
+        "Metadata",
+        lambda ji: Pretty(ji.metadata, max_length=10, max_string=500, max_depth=3),
+    ),
 }
 
 
@@ -213,7 +217,9 @@ def get_flow_info_table(flows_info: list[FlowInfo], verbosity: int) -> Table:
             row.append(", ".join(workers))
             job_states = "-".join(js.short_value for js in fi.job_states)
             row.append(job_states)
-            row.append(render_scope(fi.flow_metadata))
+            row.append(
+                Pretty(fi.flow_metadata, max_length=10, max_string=500, max_depth=3)
+            )
 
         table.add_row(*row)
 
@@ -306,9 +312,9 @@ def format_flow_info(
 ) -> Table:
     title = f"Flow: {flow_info.name} - {flow_info.flow_id} - {flow_info.state.name}"
     if verbosity > 0:
-        title += f"\nMetadata: {flow_info.flow_metadata}"
+        title += f"\nMetadata:\n{Pretty(flow_info.flow_metadata)}"
     table = get_job_info_table(
-        flow_info.jobs_info,
+        flow_info.jobs_info or [],
         verbosity=verbosity,
         output_keys=output_keys,
         stored_data_keys=stored_data_keys,
