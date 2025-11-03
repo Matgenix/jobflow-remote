ae0de7b7b2ea810f68762bf4e551bb3d9ff506c0
David Waroquiers <david.waroquiers@matgenix.com>
2025-11-03 17:44:41 +0100

    Added test for upgrade conditions.

---
+++ tests/db/jobs/test_upgrade.py
@@ -0,0 +1,61 @@
+def test_upgrade_conditions(
+    job_controller,
+    two_flows_four_jobs,
+):
+    import packaging.version
+
+    from jobflow_remote import __version__
+    from jobflow_remote.jobs.upgrade import DatabaseUpgrader, UpgradeCondition
+
+    package_version = packaging.version.parse(__version__)
+    assert job_controller.get_current_db_version() == package_version
+
+    # Register a fake upgrade to test the upgrade conditions mechanism
+    @DatabaseUpgrader.register_upgrade(
+        version="98.0",
+        upgrade_conditions=[
+            UpgradeCondition(
+                description="next_id",
+                collection="auxiliary",
+                query={"next_id": {"$exists": True}},
+                one_doc_check=lambda doc: doc["next_id"],
+            )
+        ],
+    )
+    def upgrade_to_98(*_, **__):
+        return []
+
+    @DatabaseUpgrader.register_upgrade(
+        version="99.0",
+        upgrade_conditions=[
+            UpgradeCondition(
+                description="no_jobs_ready",
+                collection="jobs",
+                query={"state": "READY"},
+            ),
+            UpgradeCondition(
+                description="no_flows_completed",
+                collection="flows",
+                query={"state": "COMPLETED"},
+            ),
+        ],
+    )
+    def upgrade_to_99(*_, **__):
+        return []
+
+    db_upgrader = DatabaseUpgrader(job_controller)
+    v98 = packaging.version.parse("98.0")
+    v99 = packaging.version.parse("99.0")
+    failed_conditions = db_upgrader.check_upgrade_conditions(versions=[v98, v99])
+    assert len(failed_conditions) == 2
+    version1, failed_condition1 = failed_conditions[0]
+    version2, failed_condition2 = failed_conditions[1]
+    assert version1 == v98
+    assert failed_condition1["condition"].description == "next_id"
+    assert failed_condition1["count"] == 5
+    assert version2 == v99
+    assert failed_condition2["condition"].description == "no_jobs_ready"
+    assert failed_condition2["count"] == 2
+
+    assert db_upgrader.upgrade(from_version=None, target_version="99.0") is False
+    assert job_controller.get_current_db_version() == package_version
